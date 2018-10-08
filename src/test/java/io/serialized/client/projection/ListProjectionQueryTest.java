e9e9caf5ed782c7f64a13ee68b2d161743d01a1e
Mattias Holmqvist <mattias@serialized.io>
2018-10-08 10:41:53 +0200

    Added tests

---
+++ src/test/java/io/serialized/client/projection/ListProjectionQueryTest.java
@@ -0,0 +1,42 @@
+package io.serialized.client.projection;
+
+import okhttp3.HttpUrl;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static io.serialized.client.projection.ListProjectionQuery.list;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ListProjectionQueryTest {
+
+  public static final HttpUrl ROOT_URL = HttpUrl.get("https://api.serialized.io");
+
+  @Test
+  public void listSimple() {
+    HttpUrl httpUrl = list("game").build(Map.class).constructUrl(ROOT_URL);
+    assertThat(httpUrl.pathSegments(), hasItems("projections", "single", "game"));
+    assertThat(httpUrl.queryParameter("limit"), nullValue());
+    assertThat(httpUrl.queryParameter("sort"), nullValue());
+  }
+
+  @Test
+  public void listWithLimit() {
+    HttpUrl httpUrl = list("game").limit(10).build(Map.class).constructUrl(ROOT_URL);
+    assertThat(httpUrl.pathSegments(), hasItems("projections", "single", "game"));
+    assertThat(httpUrl.queryParameter("limit"), is("10"));
+    assertThat(httpUrl.queryParameter("sort"), nullValue());
+  }
+
+  @Test
+  public void listWithLimitAndDescSort() {
+    HttpUrl httpUrl = list("game").limit(10).sortDescending("startTime").build(Map.class).constructUrl(ROOT_URL);
+    assertThat(httpUrl.pathSegments(), hasItems("projections", "single", "game"));
+    assertThat(httpUrl.queryParameter("limit"), is("10"));
+    assertThat(httpUrl.queryParameter("sort"), is("-startTime"));
+  }
+
+}
