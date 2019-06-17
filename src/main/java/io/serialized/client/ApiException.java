1705b17a3eb4039860080fe9440281e9babb1443
Jesper Hammarbäck <jesper@hammarback.eu>
2019-06-17 17:28:02 +0200

    Removed toString.

--- src/main/java/io/serialized/client/ApiException.java
+++ src/main/java/io/serialized/client/ApiException.java
@@ -1,9 +1,5 @@
 package io.serialized.client;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-
-import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
-
 public class ApiException extends RuntimeException {
 
   private final int statusCode;
@@ -15,11 +11,6 @@ public class ApiException extends RuntimeException {
 
   public int getStatusCode() {
     return statusCode;
-  }
-
-  @Override
-  public String toString() {
-    return ToStringBuilder.reflectionToString(this, SHORT_PREFIX_STYLE);
   }
 
 }
