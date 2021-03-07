0781c08853a0a79a124505fe864e9f06409c2979
Jesper Hammarbäck <jesper@hammarback.eu>
2021-03-07 16:14:05 +0100

    Added optional 'reference'.

--- src/main/java/io/serialized/client/projection/ProjectionRequest.java
+++ src/main/java/io/serialized/client/projection/ProjectionRequest.java
@@ -1,5 +1,7 @@
 package io.serialized.client.projection;
 
+import org.apache.commons.lang3.Validate;
+
 import java.util.UUID;
 
 public class ProjectionRequest {
@@ -7,11 +9,13 @@ public class ProjectionRequest {
   public final ProjectionType projectionType;
   public final String projectionName;
   public final UUID tenantId;
+  public final String reference;
 
   private ProjectionRequest(Builder builder) {
     this.projectionType = builder.projectionType;
     this.projectionName = builder.projectionName;
     this.tenantId = builder.tenantId;
+    this.reference = builder.reference;
   }
 
   public boolean hasTenantId() {
@@ -23,6 +27,7 @@ public class ProjectionRequest {
     private final ProjectionType projectionType;
     private String projectionName;
     private UUID tenantId;
+    private String reference;
 
     public Builder(ProjectionType projectionType) {
       this.projectionType = projectionType;
@@ -38,7 +43,13 @@ public class ProjectionRequest {
       return this;
     }
 
+    public Builder withReference(String reference) {
+      this.reference = reference;
+      return this;
+    }
+
     public ProjectionRequest build() {
+      Validate.notEmpty(projectionName, "'projectionName' must be set");
       return new ProjectionRequest(this);
     }
 
