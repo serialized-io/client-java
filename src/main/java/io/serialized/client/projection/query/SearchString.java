264355ccc0251e6cd11156f19decce6d0f424ca2
Jesper Hammarbäck <jesper@hammarback.eu>
2023-01-31 14:07:35 +0100

    Added javadoc.

--- src/main/java/io/serialized/client/projection/query/SearchString.java
+++ src/main/java/io/serialized/client/projection/query/SearchString.java
@@ -10,6 +10,18 @@ public class SearchString {
     this.string = string;
   }
 
+  /**
+   * <pre>
+   * '+' specifies AND operation: token1+token2
+   * '|' specifies OR operation: token1|token2
+   * '-' negates a single token: -token0
+   * '"' creates phrases of terms: "term1 term2 ..."
+   * '*' at the end of terms specifies prefix query: term*
+   * '(' and '' specifies precedence: token1 + (token2 | token3)
+   * '~N' at the end of terms specifies fuzzy query: term~1
+   * '~N' at the end of phrases specifies near/slop query: "term1 term2"~5
+   * </pre>
+   */
   public static SearchString string(String string) {
     Validate.notBlank(string, "Search string cannot be empty");
     return new SearchString(string);
